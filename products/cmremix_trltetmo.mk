81d97e913991af005fc4249f7c9c8b9e49174fca
ZION959 <zionmtran43@gmail.com>
2017-04-27 19:54:16 -0700

    add cmremix custom bootanimation

--- products/cmremix_trltetmo.mk
+++ products/cmremix_trltetmo.mk
@@ -1,6 +1,12 @@
 # Check for target product
 ifeq (cmremix_trltetmo,$(TARGET_PRODUCT))
 
+# Bootanimation
+PRODUCT_COPY_FILES += vendor/cmremix/prebuilt/common/bootanimation/$(CMREMIX_BOOTANIMATION_NAME).zip:system/media/bootanimation.zip
+
+# Set bootanimation Size
+CMREMIX_BOOTANIMATION_NAME := 1600
+
 # Disable debug
 # DISABLE_ADB_AUTH := true
 
