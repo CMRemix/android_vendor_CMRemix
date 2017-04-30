8f24c62ec476e4d97015b5f6d632f5b075a25beb
ZION959 <zionmtran43@gmail.com>
2017-04-29 22:38:58 -0700

    fixed

--- products/cmremix_trltetmo.mk
+++ products/cmremix_trltetmo.mk
@@ -2,10 +2,10 @@
 ifeq (cmremix_trltetmo,$(TARGET_PRODUCT))
 
 # Bootanimation
-PRODUCT_COPY_FILES += vendor/cmremix/prebuilt/common/bootanimation/$(CMREMIX_BOOTANIMATION_NAME).zip:system/media/bootanimation.zip
+#PRODUCT_COPY_FILES += vendor/cmremix/prebuilt/common/bootanimation/$(CMREMIX_BOOTANIMATION_NAME).zip:system/media/bootanimation.zip
 
 # Set bootanimation Size
-CMREMIX_BOOTANIMATION_NAME := 1600
+#CMREMIX_BOOTANIMATION_NAME := 1600
 
 # Disable debug
 # DISABLE_ADB_AUTH := true
